276e4226b17043ba1ee6e5dcff5317eb31a9dece
Arttu-S <saarelaarttu@hotmail.com>
2024-03-13 12:14:33 +0200

    updates

--- Assets/Code/CameraFollow.cs
+++ Assets/Code/CameraFollow.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Harjoituspeli
 {
-    public class Camera : MonoBehaviour
+    public class CameraFollow : MonoBehaviour
     {
 [SerializeField] public Transform target;
     public float smoothTime = 0.2F;
@@ -13,7 +13,7 @@ namespace Harjoituspeli
     void Update()
     {
         // Define a target position above and behind the target transform
-        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, 0));
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
 
         // Smoothly move the camera towards that target position
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
